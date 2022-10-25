71776895d1c126aa30a14cff8e05ed3db628f40c
Gabriela Aparecida  de Sousa Santos <104006679+aparecidagabriela@users.noreply.github.com>
2022-10-25 10:12:25 -0300

    Create anyType.ts

---
+++ modulo_02/any-type/anyType.ts
@@ -0,0 +1,31 @@
+/* eslint-disable prettier/prettier */
+/**
+ * arquivo: anyType.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre 'Tipo Any'
+ * data: 27/07/2021
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#any
+ */
+
+// Exemplo 01: Tipo Any
+const a: any = 21;
+const b: any = ['Gabriela'];
+
+const result = a + b;
+console.log(result);
+
+// Exemplo 02: Quando o tipo 'any' é inferido implícitamente
+/*let frase;
+frase = 'Oi, pessoal! Tudo bem?';
+
+console.log(frase);*/
+
+// Exemplo 03: Quando devemos usar o tipo any?!
+
+const formulario: { [campoFomulario: string]: any } = {
+  nome: 'Gabriela',
+  sobrenome: 'Sousa',
+  idade: 21,
+};
+
+console.log(formulario);
